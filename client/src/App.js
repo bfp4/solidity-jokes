f3d442f322e8cb2f3e3f90a8f8d90a300285f0fa
bfp4 <66973969+bfp4@users.noreply.github.com>
2022-01-19 15:59:49 -0500

    eject

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,18 @@
+import { useEffect, useState} from "react"
+import Web3 from "web3"
+
 function App() {
+  // const [account, setAccount] = useState(); // state variable to set account.
+  
+  // useEffect(() => {
+  //   (async () => {
+  //     const web3 = new Web3(Web3.givenProvider || 'http://localhost:7545');
+  //     const accounts = await web3.eth.requestAccounts();
+      
+  //     setAccount(accounts[0]);
+  //   })();
+  //  }, []);
+
   return (
     <div>
     </div>
