48bcbea3144b1e54f35d3eaea0a639b5ddcd9c3d
bfp4 <66973969+bfp4@users.noreply.github.com>
2022-01-19 22:37:37 -0500

    fixed bug

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,17 @@
-import { useEffect, useState} from "react"
+import { useEffect, useState } from "react"
 import Web3 from "web3"
 
 function App() {
-  // const [account, setAccount] = useState(); // state variable to set account.
+  const [account, setAccount] = useState(); // state variable to set account.
   
-  // useEffect(() => {
-  //   (async () => {
-  //     const web3 = new Web3(Web3.givenProvider || 'http://localhost:7545');
-  //     const accounts = await web3.eth.requestAccounts();
+  useEffect(() => {
+    (async () => {
+      const web3 = new Web3(Web3.givenProvider || 'http://localhost:7545');
+      const accounts = await web3.eth.requestAccounts();
       
-  //     setAccount(accounts[0]);
-  //   })();
-  //  }, []);
+      setAccount(accounts[0]);
+    })();
+   }, []);
 
   return (
     <div>
